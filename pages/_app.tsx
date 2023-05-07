d4f0d3778cb1c076b44b3c658ee441f634039bca
pal-asaga <sakiika1112@gmail.com>
2023-05-07 16:50:46 +0900

    commitしてみたapp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-// import '@/styles/globals.css'
-// import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+// import { AppProps } from 'next/dist/shared/lib/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
